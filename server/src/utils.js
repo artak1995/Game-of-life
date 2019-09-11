0ae46a9c1c431427926b6f9dbeaea1f5568715cc
Tak <artak1995@gmail.com>
2019-09-12 01:56:06 +0800

    feat: average color when there is dead cell has 3 live neighbors (#5)

--- server/src/utils.js
+++ server/src/utils.js
@@ -1,23 +1,28 @@
-export function avgcolor(color1, color2) {
-  var avg = function (a, b) { return (a + b) / 2; },
-    t16 = function (c) { return parseInt(('' + c).replace('#', ''), 16) },
-    hex = function (c) {
-      var t = (c >> 0).toString(16);
-      return t.length == 2 ? t : '0' + t
-    },
-    hex1 = t16(color1),
-    hex2 = t16(color2),
-    r = function (hex) { return hex >> 16 & 0xFF },
-    g = function (hex) { return hex >> 8 & 0xFF },
-    b = function (hex) { return hex & 0xFF },
-    res = '#' + hex(avg(r(hex1), r(hex2)))
-      + hex(avg(g(hex1), g(hex2)))
-      + hex(avg(b(hex1), b(hex2)));
-  return res;
+const hexToDecimal = (hex) => {
+  const decimal = parseInt(`0x${hex}`);
+  return decimal;
+}
+
+export const averageColor = (color1, color2) => {
+  color1 = color1.substr(1);
+  color2 = color2.substr(1);
+  const rgbR = Math.floor(((hexToDecimal(`${color1[0]}${color1[1]}`) + hexToDecimal(`${color2[0]}${color2[1]}`)) / 2)).toString(16).padStart(2, 0);
+  const rgbG = Math.floor(((hexToDecimal(`${color1[2]}${color1[3]}`) + hexToDecimal(`${color2[2]}${color2[3]}`)) / 2)).toString(16).padStart(2, 0);
+  const rgbB = Math.floor(((hexToDecimal(`${color1[4]}${color1[5]}`) + hexToDecimal(`${color2[4]}${color2[5]}`)) / 2)).toString(16).padStart(2, 0);
+  const newColor = `#${rgbR}${rgbG}${rgbB}`;
+  return newColor;
+}
+
+const generateRandomHexCode = () => {
+  const hexCode = Math.floor(Math.random() * 255).toString(16);
+  return hexCode;
 }
 
 export const generateRandomColor = (colors) => {
-  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  const randomColorR = generateRandomHexCode().padStart(2, 0);
+  const randomColorG = generateRandomHexCode().padStart(2, 0);
+  const randomColorB = generateRandomHexCode().padStart(2, 0);
+  const randomColor = `${randomColorR}${randomColorG}${randomColorB}`;
   if (colors.includes(randomColor)) {
     return generateRandomColor(colors);
   }
